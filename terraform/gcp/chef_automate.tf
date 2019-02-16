24fa1c6993f36bf3983e13716bd0f7be789fd99a
Jeff Mery <jeff@merys.net>
2019-02-16 06:59:35 -0600

    Move a2 server to own file

---
+++ terraform/gcp/chef_automate.tf
@@ -0,0 +1,45 @@
+resource "google_compute_instance" "a2" {
+  name         = "${var.automate_hostname}-${random_id.instance_id.hex}"
+  hostname     = "${local.fqdn}"
+  machine_type = "${var.automate_machine_type}"
+  zone         = "${data.google_compute_zones.available.names[0]}" // Default to first available zone
+  allow_stopping_for_update = true // Let Terraform resize on the fly if needed
+
+  labels {
+    x-contact     = "${var.label_contact}"
+    x-customer    = "${var.label_customer}"
+    x-project     = "${var.label_project}"
+    x-dept        = "${var.label_dept}"
+    x-application = "${var.label_application}"
+    x-ttl         = "${var.label_ttl}"
+  }
+
+  metadata {
+    sshKeys = "${var.label_contact}:${file("${var.gcp_ssh_public_key}")}"
+  }
+
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      type = "pd-ssd"
+      size = 100
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_dns_record_set" "a2_dns" {
+  project = "${data.google_dns_managed_zone.chef-demo.project}"
+  name = "${var.automate_hostname}.${data.google_dns_managed_zone.chef-demo.dns_name}"
+  managed_zone = "${data.google_dns_managed_zone.chef-demo.name}"
+  type = "A"
+  ttl  = 300
+
+  rrdatas = ["${google_compute_instance.a2.network_interface.0.access_config.0.nat_ip}"]
+}
