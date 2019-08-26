baf6535b35088ded9273848c896023c9fc19964e
James Massardo <jmassardo@chef.io>
2019-08-26 17:45:40 -0500

    require azurerm to use client id and secret from tfvars (#35)
    Signed-off-by: James Massardo <jmassardo@chef.io>

--- terraform/azure/resource_group.tf
+++ terraform/azure/resource_group.tf
@@ -6,6 +6,8 @@ terraform {
 provider "azurerm" {
   subscription_id         = "${var.azure_sub_id}"
   tenant_id               = "${var.azure_tenant_id}"
+  client_id               = "${var.azure_client_id}"
+  client_secret           = "${var.azure_client_secret}"
 }
 
 resource "random_id" "instance_id" {
