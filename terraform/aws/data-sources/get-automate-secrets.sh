13d94563aab0ce0f4d63470fdae6f6c928aa50b6
stellarsquall <stellarsquall@protonmail.ch>
2019-09-16 21:50:22 -0500

    added a2_ip export

--- terraform/aws/data-sources/get-automate-secrets.sh
+++ terraform/aws/data-sources/get-automate-secrets.sh
@@ -6,6 +6,8 @@ export ssh_key
 export a2_ip
 export out_path
 export origin
+export a2_token
+export a2_url
 
 eval "$(jq -r '@sh "export ssh_user=\(.ssh_user) ssh_key=\(.ssh_key) a2_ip=\(.a2_ip) out_path=\(.out_path) origin=\(.origin)"')"
 
@@ -28,4 +30,5 @@ jq -n --arg a2_admin "$a2_admin" \
       --arg a2_password "$a2_password" \
       --arg a2_token "$a2_token" \
       --arg a2_url "$a2_url" \
-      '{"a2_admin":$a2_admin,"a2_password":$a2_password,"a2_token":$a2_token,"a2_url":$a2_url}'
+      --arg a2_ip "$a2_ip" \
+      '{"a2_admin":$a2_admin,"a2_password":$a2_password,"a2_token":$a2_token,"a2_url":$a2_url, "a2_ip":$a2_ip}'
