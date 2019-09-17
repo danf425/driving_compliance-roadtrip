eefba31115b00bba42eaa89ebc0eaaeb34dd89fe
stellarsquall <stellarsquall@protonmail.ch>
2019-09-16 21:49:02 -0500

    initial commit, script for automate api call to iterate through user creation

--- terraform/aws/data-sources/create-users.sh
+++ terraform/aws/data-sources/create-users.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-export ssh_user
-export ssh_key
-export a2_ip
-export out_path
-export origin
-export a2_url
-export count
-
-eval "$(jq -r '@sh "export count=\(.count) a2_url=\(.a2_url) ssh_user=\(.ssh_user) ssh_key=\(.ssh_key) a2_ip=\(.a2_ip) out_path=\(.out_path) origin=\(.origin)"')"
-
-scp -i ${ssh_key} ${ssh_user}@${a2_ip}:/home/${ssh_user}/automate-credentials.toml $out_path/automate-credentials-${a2_ip}.toml
-
-a2_token="$(cat $out_path/automate-credentials-${a2_ip}.toml | sed -n -e 's/^api-token = //p' | tr -d '"')"
-# echo -e export TOK=$a2_token 
-
-curl -H "api-token: $a2_token" -H "Content-Type: application/json" -d '{"name":"Student Six", "username":"user006", "password":"LCRr0@dtr1p"}' https://jwolf-a2.workshops.learn.chef.io/api/v0/auth/users?pretty
+for ((i=0; i<=$USER_COUNT; i++))
+do
+  curl -k -H "api-token: $(sudo chef-automate admin-token)" -H "Content-Type: application/json" -d "{\"name\":\"student ${i}\", \"username\":\"user${i}\", \"password\":\"${USER_PASSWORD}\"}" https://$A2_HOSTNAME/api/v0/auth/users?pretty
+done
