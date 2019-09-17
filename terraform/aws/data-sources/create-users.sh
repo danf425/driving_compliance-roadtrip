5175aa8ddb07ce31f953f2f00e2e48b972262a7e
stellarsquall <stellarsquall@protonmail.ch>
2019-09-16 22:34:55 -0500

    updated for loop to increment from 1 instead of 0

--- terraform/aws/data-sources/create-users.sh
+++ terraform/aws/data-sources/create-users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-for ((i=0; i<=$USER_COUNT; i++))
+for ((i=1; i<=$USER_COUNT; i++))
 do
   curl -k -H "api-token: $(sudo chef-automate admin-token)" -H "Content-Type: application/json" -d "{\"name\":\"student ${i}\", \"username\":\"user${i}\", \"password\":\"${USER_PASSWORD}\"}" https://$A2_HOSTNAME/api/v0/auth/users?pretty
 done
