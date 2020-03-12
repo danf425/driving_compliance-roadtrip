afca69f3a0caac56572e26fd1213ab744c09e67d
stellarsquall <stellarsquall@protonmail.ch>
2020-03-12 11:32:17 -0600

    updated code-server to version 2.1698

--- terraform/aws/data-sources/setup-code-server.sh
+++ terraform/aws/data-sources/setup-code-server.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Download RPM Dependencies for Code Server
-curl http://download-ib01.fedoraproject.org/pub/fedora/linux/updates/29/Everything/x86_64/Packages/l/libstdc++-8.3.1-2.fc29.x86_64.rpm -o /tmp/libstdc++-8.3.1-2.fc29.x86_64.rpm
-curl http://archives.fedoraproject.org/pub/archive/fedora/linux/updates/28/Everything/x86_64/Packages/g/glibc-common-2.27-38.fc28.x86_64.rpm -o /tmp/glibc-common-2.27-38.fc28.x86_64.rpm
-curl http://archives.fedoraproject.org/pub/archive/fedora/linux/updates/28/Everything/x86_64/Packages/g/glibc-2.27-38.fc28.x86_64.rpm -o /tmp/glibc-2.27-38.fc28.x86_64.rpm
-curl http://archives.fedoraproject.org/pub/archive/fedora/linux/updates/28/Everything/x86_64/Packages/g/glibc-langpack-en-2.27-38.fc28.x86_64.rpm -o /tmp/glibc-langpack-en-2.27-38.fc28.x86_64.rpm
-sudo rpm -ivh --force /tmp/*.rpm
-
 # Download and install Code Server
-curl -L https://github.com/cdr/code-server/releases/download/1.939-vsc1.33.1/code-server1.939-vsc1.33.1-linux-x64.tar.gz -o /tmp/code-server1.939-vsc1.33.1-linux-x64.tar.gz
-tar xf /tmp/code-server1.939-vsc1.33.1-linux-x64.tar.gz -C /tmp
-sudo mv /tmp/code-server1.939-vsc1.33.1-linux-x64/code-server /usr/local/bin/
+curl -L https://github.com/cdr/code-server/releases/download/2.1698/code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz -o /tmp/code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz
+tar xf /tmp/code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz -C /tmp
+sudo mv /tmp/code-server2.1698-vsc1.41.1-linux-x86_64/code-server /usr/local/bin/
 
 # Install Code Server service
 sudo mv /tmp/code-server.service /etc/systemd/system/code-server.service
