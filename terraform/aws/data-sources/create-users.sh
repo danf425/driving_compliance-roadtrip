d5f98b5c6966fabd1f95c90aebfbbf28696d7a7d
Robin Beck <stellarsquall@protonmail.ch>
2020-04-23 16:35:16 -0600

    updated api call for new iam endpoints
    Signed-off-by: Robin Beck <stellarsquall@protonmail.ch>

--- terraform/aws/data-sources/create-users.sh
+++ terraform/aws/data-sources/create-users.sh
@@ -3,5 +3,6 @@ set -eu -o pipefail
 
 for ((i=1; i<=$USER_COUNT; i++))
 do
-  curl -k -H "api-token: $(sudo chef-automate iam token create demo --admin)" -H "Content-Type: application/json" -d "{\"name\":\"student ${i}\", \"username\":\"user${i}\", \"password\":\"${USER_PASSWORD}\"}" https://$A2_HOSTNAME/api/v0/auth/users?pretty
+  echo "Creating user${i}" ;
+  curl -k -H "api-token: $API_TOKEN" -H "Content-Type: application/json" -d "{\"id\":\"user${i}\",\"name\":\"student ${i}\", \"password\":\"${USER_PASSWORD}\"}" https://$A2_HOSTNAME/apis/iam/v2/users ;
 done
